205b9a5594b5cb7b4662fce9c4b6a6cf506ebb1c
Muskan Goyal <goyalmuskan210@gmail.com>
2024-10-02 17:06:08 +0530

    minor changes needed

--- main.js
+++ main.js
@@ -6,20 +6,17 @@ function showDetails(achievement) {
     if (achievement === 'atl') {
         content = `
             <h2>ATL Student Innovator</h2>
-            <p>Certified by NITI Aayog for creating a blind stick using sensors and Arduino in class 9.</p>
-            <img src="" alt="NITI Aayog Logo">
+            <p>I was certified as an ATL Student Innovator by NITI Aayog for developing a smart blind stick using sensors and Arduino, aimed at assisting visually impaired individuals with obstacle detection.</p>
         `;
     } else if (achievement === 'cbse10') {
         content = `
             <h2>Class 10 - 94%</h2>
-            <p>I scored 94% in my CBSE class 10 board exams.</p>
-            <img src="" alt="CBSE Logo">
+            <p>I achieved a score of 94% in my CBSE Class 10 board examinations.</p>
         `;
     } else if (achievement === 'cbse12') {
         content = `
             <h2>Class 12 - 90%</h2>
-            <p>I scored 90% in my CBSE class 12 board exams.</p>
-            <img src="" alt="CBSE Logo">
+            <p>I achieved a score of 90% in my CBSE Class 12 board examinations..</p>
         `;
     }
     
